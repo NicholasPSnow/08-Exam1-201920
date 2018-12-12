49d30b7a79a6df5840a9cfc93052cafb788322ae
NicholasPSnow <44656056+NicholasPSnow@users.noreply.github.com>
2018-12-11 20:53:09 -0500

    Problem 4

--- src/problem4.py
+++ src/problem4.py
@@ -3,8 +3,8 @@ Exam 1, problem 4.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Nicholas Snow.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -68,12 +68,28 @@ def problem4(number_of_stairs, step_size, starting_point, window):
       :type window:            rg.RoseWindow
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # Done: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # IMPORTANT: For PARTIAL CREDIT, you can draw just the black "bottoms"
     #            of the stair steps.
     # -------------------------------------------------------------------------
 
+    endingpoint=rg.Point(starting_point.x+(number_of_stairs*step_size),starting_point.y-(number_of_stairs*step_size))
+    endingpoint.attach_to(window)
+    starting_point.attach_to(window)
+
+    for k in range (number_of_stairs):
+        line=rg.Line(rg.Point(starting_point.x+(k*step_size),starting_point.y-(k*step_size)),rg.Point(starting_point.x+(k*step_size),starting_point.y-(k*step_size)-step_size))
+        line.color='magenta'
+        line.thickness=3
+        line.attach_to(window)
+
+        line = rg.Line(rg.Point(starting_point.x + (k * step_size), starting_point.y - (k * step_size)-step_size),rg.Point(starting_point.x + (k * step_size)+step_size, starting_point.y - (k * step_size) - step_size))
+        line.color = 'black'
+        line.thickness = 3
+        line.attach_to(window)
+
+    window.render()
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
